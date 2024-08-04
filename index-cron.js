04f83cd962090b1513b3e708652554fd89796226
Tiago Rodrigues <tiagomcrodrigues@gmail.com>
2024-08-04 19:45:20 +0100

    add ghostfolio cron

--- index-cron.js
+++ index-cron.js
@@ -1,11 +1,11 @@
-const { fixPayees, calculateMortage, init } = require("./engine.js");
+const { fixPayees, calculateMortage, ghostfolioSync } = require("./engine.js");
 var cron = require('node-cron');
 const parser = require('cron-parser');
 const { getAppConfigFromEnv } = require("./config");
 
 const appConfig = getAppConfigFromEnv();
 
-var cronExpression = "0 0 */4 * * *";
+var cronExpression = "0 */4 * * *";
 if (appConfig.CRON_EXPRESSION != "") {
     cronExpression = appConfig.CRON_EXPRESSION
 }
@@ -17,8 +17,13 @@ cron.schedule(cronExpression, async () => {
     if (appConfig.ENABLE_INTEREST_CALCULATION) {
         await calculateMortage();
     }
+
     if (appConfig.ENABLE_PAYEE_RENAME) {
         await fixPayees();
+    }
+
+    if (appConfig.ENABLE_GHSOTFOLIO_SYNC) {
+        await ghostfolioSync();
     }
     console.info('Next run:', interval.next().toISOString());
 });
