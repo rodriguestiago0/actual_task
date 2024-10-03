09cfd51ca78ce5f45f55773d9d8431c5b300f689
Tiago Rodrigues <tiagomcrodrigues@gmail.com>
2024-10-03 09:12:08 +0100

    Fix typo

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const MAIN_ACCOUNT_ID = process.env.MAIN_ACCOUNT_ID || "";
 
 const ENABLE_INTEREST_CALCULATION= process.env.ENABLE_INTEREST_CALCULATION || false;
 const ENABLE_PAYEE_RENAME = process.env.ENABLE_PAYEE_RENAME || false;
-const ENABLE_GHSOTFOLIO_SYNC = process.env.ENABLE_GHOSTFOLIO_SYNC || false;
+const ENABLE_GHOSTFOLIO_SYNC = process.env.ENABLE_GHOSTFOLIO_SYNC || false;
 const GHOSTFOLIO_SERVER_URL = process.env.GHOSTFOLIO_SERVER_URL || "";
 const GHOSTFOLIO_TOKEN = process.env.GHOSTFOLIO_TOKEN || "";
 
@@ -24,6 +24,7 @@ const CRON_EXPRESSION = process.env.CRON_EXPRESSION || "0 */4 * * *";
 const ACTUAL_SYNC_ID = process.env.ACTUAL_SYNC_ID || "";
 
 const ENABLE_HOLD_INCOME_FOR_NEXT_MONTH = process.env.ENABLE_HOLD_INCOME_FOR_NEXT_MONTH || false;
+
 
 function validateEnv(variables){
     // Assert that all required environment variables are set
@@ -60,7 +61,7 @@ function getAppConfigFromEnv() {
     
     GHOSTFOLIO_ACCOUNT_MAPPING = {}
     GHOSTFOLIO_ACTUAL_PAYEE_NAME_MAPPING = {}
-    if (ENABLE_GHSOTFOLIO_SYNC) {
+    if (ENABLE_GHOSTFOLIO_SYNC) {
         
         ghostfolioAccount = process.env.GHOSTFOLIO_ACCOUNT
         actualAccount = process.env.GHOSTFOLIO_ACTUAL_ACCOUNT
@@ -112,11 +113,12 @@ function getAppConfigFromEnv() {
         INTEREST_RATE,
         ENABLE_INTEREST_CALCULATION,
         ENABLE_PAYEE_RENAME,
-        ENABLE_GHSOTFOLIO_SYNC,
+        ENABLE_GHOSTFOLIO_SYNC,
         GHOSTFOLIO_ACCOUNT_MAPPING,
         GHOSTFOLIO_ACTUAL_PAYEE_NAME_MAPPING,
         GHOSTFOLIO_SERVER_URL,
         GHOSTFOLIO_TOKEN,
+        ENABLE_HOLD_INCOME_FOR_NEXT_MONTH
     }
 
     return appConfig
--- index-cron.js
+++ index-cron.js
@@ -13,20 +13,40 @@ console.info("Defined cron is: ", cronExpression)
 const interval = parser.parseExpression(cronExpression);
 console.info('Next run:', interval.next().toISOString());
 
+if (appConfig.ENABLE_INTEREST_CALCULATION) {
+    console.info("Task interest calculation enabled")
+}
+
+if (appConfig.ENABLE_PAYEE_RENAME) {
+    console.info("Task fix payees enabled")
+}
+
+if (appConfig.ENABLE_GHOSTFOLIO_SYNC) {
+    console.info("Task ghostfolio sync enabled")
+}
+
+if (appConfig.ENABLE_HOLD_INCOME_FOR_NEXT_MONTH){
+    console.info("Task hold for next month enabled")
+}
+
 cron.schedule(cronExpression, async () => {
     if (appConfig.ENABLE_INTEREST_CALCULATION) {
+        console.info("Running interest calculation")
         await calculateMortage();
     }
 
     if (appConfig.ENABLE_PAYEE_RENAME) {
+        console.info("Running fix payees")
         await fixPayees();
     }
 
-    if (appConfig.ENABLE_GHSOTFOLIO_SYNC) {
+    if (appConfig.ENABLE_GHOSTFOLIO_SYNC) {
+        console.info("Running ghostfolio sync")
         await ghostfolioSync();
     }
 
     if (appConfig.ENABLE_HOLD_INCOME_FOR_NEXT_MONTH){
+        console.info("Running hold for next month")
         await holdAmoutForNextMonth();
     }
     console.info('Next run:', interval.next().toISOString());
